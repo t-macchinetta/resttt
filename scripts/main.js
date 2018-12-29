b6f164d098f18abf6d7335aac4276488892b9415
taroosg <macchinetta.xyz@gmail.com>
2018-12-30 00:05:24 +0900

    fix small

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,36 @@
+'use strict';
+var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+    window.location.hostname === '[::1]' ||
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+if ('serviceWorker' in navigator &&
+    (window.location.protocol === 'https:' || isLocalhost)) {
+    navigator.serviceWorker.register('service-worker.js')
+        .then(function (registration) {
+            registration.onupdatefound = function () {
+                if (navigator.serviceWorker.controller) {
+                    var installingWorker = registration.installing;
+                    installingWorker.onstatechange = function () {
+                        switch (installingWorker.state) {
+                            case 'installed':
+                                break;
+                            case 'redundant':
+                                throw new Error('The installing ' +
+                                    'service worker became redundant.');
+                            default:
+                        }
+                    };
+                }
+            };
+        }).catch(function (e) {
+            console.error('Error during service worker registration:', e);
+        });
+}
+
+
 // test用
 // const latlng = { lat: 33.5867315, lng: 130.3945875 };
 
